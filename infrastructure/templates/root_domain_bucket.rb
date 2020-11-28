3d56367d4868732d62c29349cb9e9702b4b2de5d
Brienna Macnish <brienna.macnish@envato.com>
2020-11-28 16:55:13 +1100

    Remove website configuration and access policy on root_domain

--- infrastructure/templates/root_domain_bucket.rb
+++ infrastructure/templates/root_domain_bucket.rb
@@ -21,38 +21,11 @@ SparkleFormation.new(:root_domain_bucket) do
     deletion_policy 'Retain'
     properties do
       bucket_name ref!(:domain)
-      website_configuration do
-        index_document ref!(:index_document)
-      end
       logging_configuration do
         destination_bucket_name ref!(:logs_bucket)
         log_file_prefix 'logs/'
       end
     end
-  end
-
-  resources.bucket_policy do
-    type 'AWS::S3::BucketPolicy'
-    properties do
-      bucket ref!(:domain)
-      policy_document do
-        version '2012-10-17'
-        statement array!(
-          lambda {
-            sid 'PublicReadGetObject'
-            effect 'Allow'
-            principal '*'
-            action 's3:GetObject'
-            resource ['arn:aws:s3:::briennamacnish.com/*']
-          },
-        )
-      end
-    end
-  end
-
-  outputs.website_endpoint do
-    description 'Url '
-    value get_att!(:root_domain_bucket, 'WebsiteURL')
   end
 
   outputs.root_domain do
